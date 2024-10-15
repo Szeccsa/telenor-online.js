e49e1b9e0883a5fa700770f4779ce89f15f0e8f9
Kevin Király <95046202+Szeccsa@users.noreply.github.com>
2024-10-15 15:40:50 +0200

    Vanilla instead of Jquery...

--- telenor-online.js
+++ telenor-online.js
@@ -1,23 +1,33 @@
-// This script basically creates a cookie if the user clicks on the login button. No framework or backend used. Made by Szeccsa.
+// This script is basically creates a cookie if the user clicks on the login button. No framework or backend used. Made by Szeccsa.
 function setCookie(name, value, days) {
-    var expires = new Date(Date.now() + days * 864e5).toUTCString();
-    document.cookie = name + "=" + encodeURIComponent(value) + "; expires=" + expires + "; path=/";
+    const expires = new Date(Date.now() + days * 864e5).toUTCString();
+    document.cookie = `${name}=${encodeURIComponent(value)}; expires=${expires}; path=/`;
 }
 
 function deleteCookie(name) {
-    document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+    document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
 }
 
-$(document).ready(function() {
-    $("button[type='submit']").on("click", function(event) {
-        event.preventDefault();
-        setCookie("tolid", "0", 7); // Stay logged in for 7 days
-        location.reload();
-    });
+function handleLoginButtonClick(event) {
+    event.preventDefault();
+    setCookie("tolid", "0", 7); // Stay logged in for 7 days
+    location.reload();
+}
 
-    $("li a[href='#'], a.black-button[href='#']").on("click", function(event) {
-        event.preventDefault();
-        deleteCookie("tolid"); // Delete the cookie
-        location.reload();
+function handleLogoutClick(event) {
+    event.preventDefault();
+    deleteCookie("tolid"); // Delete the cookie
+    location.reload();
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const loginButton = document.querySelector("button[type='submit']");
+    if (loginButton) {
+        loginButton.addEventListener("click", handleLoginButtonClick);
+    }
+
+    const logoutLinks = document.querySelectorAll("li a[href='#'], a.black-button[href='#']");
+    logoutLinks.forEach(link => {
+        link.addEventListener("click", handleLogoutClick);
     });
 });
